9a281516d44a8f1c4d69df3378c2606368c28221
Akshara Rai <akshara.rai@gmail.com>
2018-04-13 21:31:20 -0400

    kernel 5d kinda working

--- Atrias/Atrias_bo/optimize_kernel_bo.m
+++ Atrias/Atrias_bo/optimize_kernel_bo.m
@@ -8,34 +8,30 @@ warning off all
 % Example command to launch a few runs:
 % matlab -nodisplay -r "grun=1:5; disturb=1; hwadjust=1; cost_op_str='findCost_metabolic'; optimize_kernel_bo"
 if ~exist('grun', 'var'), grun=1; end
-if ~exist('disturb', 'var'), disturb=0; end
 if ~exist('hwadjust', 'var'), hwadjust=0; end
 if ~exist('cost_op_str', 'var'), cost_op_str='findCost_metabolic'; end
 if ~exist('max_iters', 'var'), max_iters=50; end
 
 extras_str = '';
-if disturb, extras_str = char(strcat(extras_str, '_disturb')); end
 if hwadjust, extras_str = char(strcat(extras_str, '_hwadjust', num2str(hwadjust))); end
 
 %% Model name and dimensionality
 
-model_char = 'original';
-dog_model_char = 'original';
+model_char = 'original'; %model in which optimization are run
+dog_model_char = 'original'; %model from which kernel 
 model = strcat('atrias_', model_char);
-stopTime = 5.0;
-dim = 50;
+dim = 5;
 
 mkdir(['tmp_kernel_bo',num2str(grun(1)),'_', num2str(grun(end)),extras_str])
 cwd = pwd;
 cd(['tmp_kernel_bo',num2str(grun(1)),'_', num2str(grun(end)),extras_str])
 addpath(genpath('../'))
 addpath(genpath(['../../../bayesopt']))
-rmpath(genpath('../../../bayesopt/gpml'));  % TODO(rika): remove after update
-addpath(genpath('../../../bayesopt/private/gpml_2017'));  % TODO(rika): remove
 
 siminit_name = strcat(['SimInitial_',model_char,'_tolstoy.mat']);
 if ~fake
-	[f1, f2, s, s_4d, simout_vec] = main_ATRIAS_cluster(model, stopTime, dim, siminit_name);
+    tmp_stopTime = 5.0;
+	[f1, f2, s, s_4d, simout_vec] = main_ATRIAS_cluster(model, tmp_stopTime, dim, siminit_name);
 	fprintf("costs 1 and 2 - %f and %f, score = %f, score_4d", f1, f2, s); s_4d
 end
 
@@ -49,7 +45,7 @@ opt.optimize_ei = 0;  % TODO: we should use optimize_ei for high-dim BO!
 opt.dims = dim;
 
 opt.max_iters = max_iters;
-opt.hwadjust_signal_threshold = 50;
+opt.hwadjust_signal_threshold = 50; % choose a threshold of function value variance to start optimizing hyperparameters
 opt.hwadjust_scale = 10; % hyperparameter to weigh down the importance of mismatch terms - ideally should be learned
 %%
 if(dim == 5)
@@ -58,22 +54,21 @@ if(dim == 5)
     xLimitHigh = xLimitHigh.xLimitHigh;
     xLimitLow = load('xLimitLow5d.mat');
     xLimitLow = xLimitLow.xLimitLow;
+    num_grid_points = 20000;
 elseif (dim == 9)
     z = load('grid_9d.mat');
     xLimitHigh = load('xLimitHigh9d.mat');
     xLimitHigh = xLimitHigh.xLimitHigh;
     xLimitLow = load('xLimitLow9d.mat');
     xLimitLow = xLimitLow.xLimitLow;
+    num_grid_points = 100000;
 else 
     z = load('grid_50d.mat');
     xLimitHigh = 1.25*ones(1,50);
     xLimitLow = 0.75*ones(1,50);
+    num_grid_points = 200000;
 end
-if opt.optimize_ei == 0
-    opt.grid = z.xgrid(1:200000,:);
-else
-    opt.grid = z.xgrid(1:10000,:);
-end
+opt.grid = z.xgrid(1:num_grid_points,:);
 opt.grid_size = size(opt.grid,1);
 opt.mins = xLimitLow;
 opt.maxes = xLimitHigh;
@@ -82,15 +77,13 @@ opt.maxes = xLimitHigh;
 % simulation-based score obatined from 5s runs.
 opt.stopTime = 30;
 opt.sim_stopTime = 5;  % 10s simulations
-num_rand_pts = 2;  % 2 points seems safe for 50D
 
 %% Load simulation-based DoG scores and construct the kernel from them.
-tic
 locomapF = str2func('locomap');
 dog_model = strcat('atrias_', dog_model_char);
 load(['data/data_comparisons/',dog_model,'score_1_',num2str(dim),'d.mat']);
 dog_scores = scores_1d;
-dog_scores(dog_scores < -10) = -10;
+dog_scores(dog_scores < -10) = -10; %saturate dog scores from sim errors
 if hwadjust >= 1
     opt.hwadjust = hwadjust;
     opt.hwadjust_orig_sim_dog_scores = dog_scores;
@@ -113,9 +106,12 @@ else
     opt.kernel_dims = size(dog_scores_map,2);
 end
 ddummy = @(q) 1;  % TODO: handle derivatives better
-opt.covfunc = {@covWarp,{@covSEard},locomapF,ddummy,opt.kernel_dims};
+opt.covfunc = {@covWarp,{@covSEard},locomapF,ddummy,opt.kernel_dims}; 
+%gpml covWarp takes a standard kernel, transform function, transform
+%derivative, and final kernel dimensions as input
 
 %% BO iterations
+num_rand_pts = 2;  % 2 points seems safe for 50D
 botrace_kernel = {};
 for i = 1:length(grun)
     fprintf('------ DoG kernel BO run %d ------\n', grun(i));
@@ -124,7 +120,6 @@ for i = 1:length(grun)
         f, opt, model, cost_op, num_rand_pts, locomapF, siminit_name);
     botrace_kernel{i} = botrace;
 end
-toc
 %go back to initial folder
 cd(cwd)
 %% Save botrace data
--- bayesopt/locomap.m
+++ bayesopt/locomap.m
@@ -6,37 +6,44 @@
 % the_map persists between calls to locomap(), but is accessible only to 
 % locomap() function (so not a global variable).
 %
-% If only inx is specified, for each row of inx, the function constructs 
-% the key and looks it up in the_map.
+% If only inx is specified, for each row of inx, the function looks it up in the_map.
 %
 % If inx and newx are specified, for each row of inx, the function constructs 
 % the key and updates: the_map(mkey) = newx(i,:)
+%
+% the_map: map of controller to kernel value
+% the_map_dim : dimension of map output - gpml needs this. Usually 1 or 2
+% the_out_diff : % should the output be the difference of 2 dims. true in
+% hwadjust = 2, false in all other cases
+% outx is the feature transform for inx
 function [outx] = locomap(inx, newx, init_grid, init_data, out_diff)
-  persistent the_map;
+  persistent the_map; 
   persistent the_map_dim;
-  persistent the_out_diff;
+  persistent the_out_diff; 
   outx = NaN;
-  if nargin >= 4
+  if nargin<5, out_diff = false; end
+  if nargin >= 4 %initialize map
     fprintf('locomap(): loading %d entries\n', size(init_data, 1))
     assert(size(init_data, 1) == size(init_grid,1));
     the_map = containers.Map;
     the_map_dim = size(init_data,2);
     the_out_diff = false;
-    if nargin == 5  % use k^{v_0}_{DoG_{adjust}}
+    if out_diff  % use k^{v_0}_{DoG_{adjust}}
         assert(size(init_data,2) == 2);  % 2nd dim contains \bar{g}_{new}
         the_map_dim = 1;
         the_out_diff = out_diff;
     end
+    % generate the map from data
     for i = 1:size(init_grid,1)
         mkey = sprintf('%0.4g', init_grid(i,:));
         the_map(mkey) = init_data(i,:);
     end
-  elseif nargin == 2
+  elseif nargin == 2 %update map from hardware data
       for i = 1:size(inx,1)
         mkey = sprintf('%0.4g', inx(i,:));
         the_map(mkey) = newx(i,:);
       end
-  elseif nargin == 1
+  elseif nargin == 1 %query map for feature transform
       outx = nan([size(inx,1),the_map_dim]);
       for i = 1:size(inx,1)
         mkey = sprintf('%0.4g', inx(i,:));
